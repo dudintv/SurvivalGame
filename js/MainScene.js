b4cd79cf7e61590e78ee8232066986809f0403b8
Dmitry Dudin <ddudin@harlemnext.com>
2021-11-02 11:40:49 +0100

    add resources on the map

--- js/MainScene.js
+++ js/MainScene.js
@@ -1,4 +1,5 @@
 import Player from "./Player.js";
+import Resource from "./Resource.js";
 
 export default class MainScene extends Phaser.Scene{
   constructor(){
@@ -8,6 +9,7 @@ export default class MainScene extends Phaser.Scene{
   preload(){
     console.log('PRELOAD');
     Player.preload(this);
+    Resource.preload(this);
     this.load.image('tiles', 'assets/images/RPG Nature Tileset.png');
     this.load.tilemapTiledJSON('map', 'assets/images/map.json');
   }
@@ -15,12 +17,15 @@ export default class MainScene extends Phaser.Scene{
   create(){
     console.log('CREATE');
     const map = this.make.tilemap({key: 'map'});
+    this.map = map;
     const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
     const layer1 = map.createLayer('Tile Layer 1', tileset, 0, 0);
     const layer2 = map.createLayer('Tile Layer 2', tileset, 0, 0);
 
     layer1.setCollisionByProperty({collides: true});
     this.matter.world.convertTilemapLayer(layer1);
+
+    this.addResources();
 
     this.player = new Player({scene: this, x: 200, y: 120, texture: 'male', frame:'townsfolk_m_idle_1'});
     const testPlayer = new Player({scene: this, x: 300, y: 100, texture: 'male', frame:'townsfolk_m_idle_1'});
@@ -29,6 +34,13 @@ export default class MainScene extends Phaser.Scene{
       down: Phaser.Input.Keyboard.KeyCodes.S,
       left: Phaser.Input.Keyboard.KeyCodes.A,
       right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
+  }
+
+  addResources(){
+    const resources = this.map.getObjectLayer('Resources');
+    resources.objects.forEach(resource => {
+      const item = new Resource({ scene: this, resource });
     });
   }
 
---
+++ js/Resource.js
@@ -0,0 +1,21 @@
+export default class Resource extends Phaser.Physics.Matter.Sprite {
+  static preload(scene){
+    scene.load.atlas('resources', 'assets/images/resources.png', 'assets/images/resources_atlas.json');
+  }
+
+  constructor(data){
+    const {scene, resource} = data;
+    super(scene.matter.world, resource.x, resource.y, 'resources', resource.type);
+    this.scene.add.existing(this);
+
+    const { Bodies } = Phaser.Physics.Matter.Matter;
+    const yOrigin = resource.properties.find(p => p.name === 'yOrigin').value;
+    this.x += this.width/2;
+    this.y -= this.height/2;
+    this.y += this.height*(yOrigin - 0.5);
+    const circleCollider = Bodies.circle(this.x, this.y, 12, { isSensor: false, label: 'collider' });
+    this.setExistingBody(circleCollider);
+    this.setStatic(true);
+    this.setOrigin(0.5, yOrigin);
+  }
+}
